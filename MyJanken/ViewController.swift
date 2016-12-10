4056c58e9f0b82bf2dd4606d47df3a6dbea9af8d
nao <mezase.nember.1@gmail.com>
2016-12-11 04:36:06 +0900

    Initial Commit

---
+++ MyJanken/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyJanken
+//
+//  Created by nao on 2016/12/11.
+//  Copyright © 2016年 com.naoya.MyJanken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
